4fc928dece9bb9b7fd2b786ee4b7b488b5ec17e5
Sunny Hirai <sunny.hirai@orbs.com>
2014-11-03 15:18:57 -0800

    Bumped gulp-react version

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var bundle = require('./bundle');
 
 gulp.task('default', function() {
+  var reactBundle = bundle('./js/react.js', './build/react.js');
   var alphaBundle = bundle('./js/alpha.js', './build/alpha.js');
   var bravoBundle = bundle('./js/bravo.js', './build/bravo.js', alphaBundle);
   var joinBundle = bundle(['./js/alpha.js', './js/bravo.js'], './build/join.js', alphaBundle);
---
+++ js/react.js
@@ -0,0 +1,17 @@
+/**
+ * @jsx React.DOM
+ */
+
+var React = require('react');
+
+var App = React.createClass({
+
+  render: function() {
+    return (
+      <div />
+    );
+  }
+
+});
+
+module.exports = App;
